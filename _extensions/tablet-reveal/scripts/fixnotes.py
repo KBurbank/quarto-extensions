ce089171695aca78a3ad20125f62077380bb7d3c
Kendra Burbank <burbank@uchicago.edu>
2024-03-19 13:49:24 -0500

    now only revealjs files get extra notes versions

--- _extensions/tablet-reveal/scripts/fixnotes.py
+++ _extensions/tablet-reveal/scripts/fixnotes.py
@@ -3,19 +3,22 @@
 import os
 def replace_show_notes_in_files(filenames):
     for filename in filenames:
+        print(filename)
         if filename.endswith(".html") and not filename.endswith("-speaker.html"):
+
             with open(filename, 'r') as file:
                 file_data = file.read()
 
-            file_data = file_data.replace("showNotes: false,", "showNotes: true,")
+            if "showNotes: false" in file_data:
+                file_data = file_data.replace("showNotes: false,", "showNotes: true,")
 
-            base, ext = os.path.splitext(filename)
-            new_filename = f"{base}-notes{ext}"
+                base, ext = os.path.splitext(filename)
+                new_filename = f"{base}-notes{ext}"
 
-            with open(new_filename, 'w') as file:
-                file.write(file_data)
+                with open(new_filename, 'w') as file:
+                    file.write(file_data)
 
 # Get filenames from QUARTO_PROJECT_OUTPUT_FILES environment variable
-filenames = os.environ['QUARTO_PROJECT_OUTPUT_FILES'].split('\n')
 
+filenames = os.environ['QUARTO_PROJECT_OUTPUT_FILES'].split('\n')
 replace_show_notes_in_files(filenames)
