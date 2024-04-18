56a7cf7253a72a0f022594de96be2992b40e0731
Kendra Burbank <burbank@uchicago.edu>
2024-04-17 20:51:26 -0500

    stopped checking for revealjs format (why?)

--- _extensions/hw-no-solutions/no_solutions.lua
+++ _extensions/hw-no-solutions/no_solutions.lua
@@ -1,15 +1,16 @@
 
 function Div(elem)
         local classes = elem.classes
-        if not quarto.doc.is_format("revealjs") then
+   
             for _, class in ipairs(classes) do
-                if class == "solutions" then
+                if (class == "solutions") then
                     -- remove this div
                     return {}
                 end
                 
+                
             end
-        end
+
         -- if we haven't returned yet, keep the div
         return elem
     end
